4f993f7770924a4defe487db2d2135d90484833e
MinaPhan2815 <minhnhatphan2815@gmail.com>
2023-11-06 11:40:22 +0700

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,29 +27,6 @@ for (let i = 0; i < navElemArr.length; i++) {
 }
 
 
-
-/**
- * header sticky
- */
-
-const header = document.querySelector("[data-header]");
-
-let lastScrollPosition = 0;
-
-window.addEventListener("scroll", function () {
-
-  let scrollPosition = window.pageYOffset;
-
-  if (scrollPosition > lastScrollPosition) {
-    header.classList.remove("active");
-  } else {
-    header.classList.add("active");
-  }
-
-  lastScrollPosition = scrollPosition <= 0 ? 0 : scrollPosition;
-
-});
-
 const date = document.querySelector('#date')
 const currentYear = new Date().getFullYear()
 date.textContent = currentYear
